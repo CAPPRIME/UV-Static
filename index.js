04c0b32a07e3bbc550b4d77b654e0897e320f373
CAP <159663130+CAPPRIME@users.noreply.github.com>
2025-04-14 19:15:24 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const routes = {
+  '/mastery': './static/loader.html',
+  '/apps': './static/apps.html',
+  '/gms': './static/gms.html',
+  '/lessons': './static/agloader.html',
+  '/info': './static/info.html',
+  '/mycourses': './static/loading.html',
+  '/2': './active/index.html'
+};
